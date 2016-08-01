bac007e29b2647ffd5a2626717cb24c066c22f7e
Leon <xiaoyankeji@hotmail.com>
2016-08-01 13:55:45 +0800

    第一次提交

---
+++ 屏幕共享端/ShareForm.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Drawing.Imaging
+Imports System.Net.Sockets
+Imports System.Threading
+
+Public Class ShareForm
+    Dim ConnectPort As Integer = 12345
+    Dim RemoteIP As String = "localhost"
+    Dim UDPClient As New UdpClient()
+    Dim ShareThread As Thread
+
+    Private Sub ShareForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ShareThread = New Thread(AddressOf ShareScreen)
+        'ShareThread.Start()
+    End Sub
+
+    Private Sub ShareScreen()
+        Dim ScreenBitmap As Bitmap = New Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height)
+        Dim ScreenGraphics As Graphics = Graphics.FromImage(ScreenBitmap)
+        Dim EmptyPoint As Point = New Point(0, 0)
+        Dim BitmapStream As IO.MemoryStream = New IO.MemoryStream()
+        Dim TempBytes(255) As Byte
+        While True
+            ScreenGraphics.CopyFromScreen(EmptyPoint, EmptyPoint, Screen.PrimaryScreen.Bounds.Size)
+            ScreenBitmap.Save(BitmapStream, ImageFormat.Jpeg)
+            Dim BitmapByte() As Byte = BitmapStream.GetBuffer
+            Debug.Print("- 发送端：即将发送的数据长度：" & BitmapStream.Length)
+            UDPClient.Send(BitmapByte, BitmapByte.Length, RemoteIP, ConnectPort)
+            Debug.Print("- 发送端：成功发送。")
+            Thread.Sleep(1000)
+        End While
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ScreenBitmap As Bitmap = New Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height)
+        Dim ScreenGraphics As Graphics = Graphics.FromImage(ScreenBitmap)
+        Dim EmptyPoint As Point = New Point(0, 0)
+        Dim BitmapStream As IO.MemoryStream = New IO.MemoryStream()
+        ScreenGraphics.CopyFromScreen(EmptyPoint, EmptyPoint, Screen.PrimaryScreen.Bounds.Size)
+        ScreenBitmap.Save(BitmapStream, ImageFormat.Jpeg)
+        Dim BitmapByte() As Byte = BitmapStream.GetBuffer
+        Dim TempBytes(255) As Byte
+
+        Debug.Print("- 发送端：即将发送的数据长度：" & BitmapStream.Length)
+        UDPClient.Send(BitConverter.GetBytes(BitmapStream.Length), 8, RemoteIP, ConnectPort)
+
+        While True
+            Dim TempLength As Integer
+            TempLength = BitmapStream.Read(TempBytes, 0, TempBytes.Length)
+            If TempLength <= 0 Then Exit While
+            UDPClient.Send(TempBytes, TempLength, RemoteIP, ConnectPort)
+        End While
+
+        Debug.Print("- 发送端：成功发送。")
+        UDPClient.Close()
+    End Sub
+End Class
---
+++ 屏幕监视端/MonitorForm.vb
@@ -0,0 +1,43 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Threading
+
+Public Class MonitorForm
+    Dim ConnectPort As Integer = 12345
+    Dim UDPClient As New UdpClient(ConnectPort)
+    Dim MonitorThread As Thread
+
+    Private Sub MonitorForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MonitorThread = New Thread(AddressOf ReceiveScreen)
+        MonitorThread.Start()
+    End Sub
+
+    Private Sub ReceiveScreen()
+        While True
+            Dim RemoteIpEndPoint As New IPEndPoint(IPAddress.Any, ConnectPort)
+            Dim BitmapStream As MemoryStream = New MemoryStream()
+            Dim DataLengthBytes(7) As Byte '用于接收图像数据的长度
+            DataLengthBytes = UDPClient.Receive(RemoteIpEndPoint)
+            Dim DataLength As Long = BitConverter.ToInt64(DataLengthBytes, 0)
+            Debug.Print("+ 接收端：即将接收的数据长度：" & DataLength)
+
+            While True
+                Dim TempBytes() As Byte = UDPClient.Receive(RemoteIpEndPoint)
+                Dim TempLength As Integer = TempBytes.Length
+                If TempLength <= 0 Then Exit While
+                Debug.Print("收到分组长度：" & TempLength)
+                BitmapStream.Write(TempBytes, 0, TempLength)
+            End While
+
+            Debug.Print("+ 接收端：成功接收长度：" & DataLength)
+            Me.BackgroundImage = Bitmap.FromStream(BitmapStream)
+            Debug.Print("+ 接收端：完成图像显示。")
+
+            'MsgBox(vbNullString,, RemoteIpEndPoint.Address.ToString() & ":" & RemoteIpEndPoint.Port)
+
+            UDPClient.Close()
+        End While
+    End Sub
+
+End Class
